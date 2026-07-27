3662e53f15ada61508660d3390f668dafe23c952
Harinisrivalli <sriharinivadivel@gmail.com>
2026-07-27 22:50:29 +0530

    Doctor Updated Successfully

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import ErrorPage from './components/Error.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
 import { lazy } from 'react'
 
 const DashBoard = lazy(() => import("./components/DashBoard.jsx"));
